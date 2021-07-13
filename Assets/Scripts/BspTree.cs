0d0b4fd841a21ca0dfd406847c32bc2896fce967
Alexander <Ikunkka92@gmail.com>
2021-07-13 17:43:56 +0300

    generate map

---
+++ Assets/Scripts/BspTree.cs
@@ -0,0 +1,146 @@
+﻿using UnityEngine;
+
+namespace TerribleDungeon
+{
+    public class BspTree
+    {
+        public RectInt container;
+        public RectInt room;
+        public BspTree left;
+        public BspTree right;
+        public int bspTreeId;
+
+        public  static int MIN_ROOM_SIZE = 4;
+
+        public static int debugId;
+
+        public int currentId;
+
+        private const int MIN_ROOM = 5;
+
+        public BspTree(RectInt a)
+        {
+            container = a;
+            currentId = debugId++;
+        }
+
+        internal static BspTree Split(int numberOfOperations, RectInt container)
+        {
+            var node = new BspTree(container);
+
+            if (numberOfOperations == 0)
+            {
+                return node;
+            }
+
+            var splitedContainer = SplitContainer(container);
+
+            node.left = Split(numberOfOperations - 1, splitedContainer[0]);
+
+            node.right = Split(numberOfOperations - 1, splitedContainer[1]);
+
+            return node;
+        }
+
+        private static RectInt[] SplitContainer(RectInt container)
+        {
+            RectInt c1, c2;
+            if (container.width < MIN_ROOM)
+            {
+                c1 = new RectInt(0, 0, 0, 0);
+                c2 = new RectInt(0, 0, 0, 0);
+
+                return new RectInt[] { c1, c2 };
+            }
+            if (container.height < MIN_ROOM)
+            {
+                c1 = new RectInt(0, 0, 0, 0);
+                c2 = new RectInt(0, 0, 0, 0);
+
+                return new RectInt[] { c1, c2 };
+            }
+
+            bool horizontal;
+            if (container.width / container.height >= 1.25f)
+            {
+                horizontal = true;
+            }
+            else if (container.height / container.width >= 1.25f)
+            {
+                horizontal = false;
+            }
+            else
+            {
+                horizontal = Random.Range(0f, 1f) > 0.5f ? true : false;
+            }
+
+            if (horizontal)
+            {
+                c1 = new RectInt(container.x, container.y, (int)Random.Range(container.width * 0.3f, container.width * 0.5f), container.height);
+                c2 = new RectInt(container.x + c1.width, container.y, container.width - c1.width, container.height);
+            }
+            else
+            {
+                c1 = new RectInt(container.x, container.y, container.width, (int)Random.Range(container.height * 0.3f, container.height * 0.5f));
+                c2 = new RectInt(container.x, container.y + c1.height, container.width, container.height - c1.height);
+            }
+            return new RectInt[] { c1, c2 };
+        }
+
+        public bool IsLeaf()
+        {
+            return left == null && right == null;
+        }
+
+        public bool IsInternal()
+        { // de morgan's
+            return left != null || right != null;
+        }
+
+        public static void GenerateRoomInsideContainersNode(BspTree node)
+        {
+            if (node.left == null && node.right == null)
+            {
+                //Debug.Log("moy current id " + node.currentId + " moy debugId " + BspTree.debugId);
+                var randomX = Random.Range(MIN_ROOM_SIZE, node.container.width / 4);
+                var randomY = Random.Range(MIN_ROOM_SIZE, node.container.height / 4);
+                var x = node.container.x + randomX;
+                var y = node.container.y + randomY;
+                var widthRoom = node.container.width - randomX;// - (int)(randomX * Random.Range(1f, 1.5f));
+                                                               //var widthRoom = node.container.width  - (int)(randomX * Random.Range(0.5f, 1.1f));
+                var heightRoom = node.container.height - randomY;// - (int)(randomY * Random.Range(1f, 1.5f));
+                                                                 //var heightRoom = node.container.height - (int)(randomY * Random.Range(0.5f, 1.1f));
+
+                //if (widthRoom > node.container.width)
+                //{
+                //    widthRoom = (int)(widthRoom * Random.Range(0.3f, 0.6f));
+                //}
+                //if (heightRoom > node.container.width)
+                //{
+                //    heightRoom = (int)(heightRoom * Random.Range(0.3f, 0.6f));
+                //}
+                //if (widthRoom < MIN_ROOM_SIZE)
+                //{
+                //    widthRoom = MIN_ROOM_SIZE;
+                //}
+                //if (heightRoom < MIN_ROOM_SIZE)
+                //{
+                //    heightRoom = MIN_ROOM_SIZE;
+                //}
+                node.room = new RectInt(x, y, widthRoom, heightRoom);
+            }
+            else
+            {
+                if (node.left != null)
+                {
+                    GenerateRoomInsideContainersNode(node.left);
+                }
+                if (node.right != null)
+                {
+                    GenerateRoomInsideContainersNode(node.right);
+                }
+            }
+        }
+    }
+}
+     
---
+++ Assets/Scripts/GenerateMapScript.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TerribleDungeon
+{
+    public class GenerateMapScript : MonoBehaviour
+    {
+        List<BspTree> allLevels;
+
+        BspTree dungeonTree;
+
+        int[,] worldMap;
+        int[,] borderedMap;
+
+        public int widthDungeon;
+        public int heightDungeon;
+        public int numberOfOperations;
+
+        void Start()
+        {
+            GenerateMap();
+        }
+
+        public void GenerateMap()
+        {
+            
+            worldMap = new int[widthDungeon, heightDungeon];
+
+            for (int x = 0; x < worldMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < worldMap.GetLength(1); y++)
+                {
+                    worldMap[x, y] = 1;
+                }
+            }
+
+            RectInt dungeonRect = new RectInt(0, 0, widthDungeon, heightDungeon);
+
+            dungeonTree = BspTree.Split(numberOfOperations, dungeonRect);
+            BspTree.GenerateRoomInsideContainersNode(dungeonTree);
+            GenerateArrayOfMap(dungeonTree);
+
+            int borderSize = 10;
+            borderedMap = new int[widthDungeon + borderSize * 2, heightDungeon + borderSize * 2];
+            for (int x = 0; x < borderedMap.GetLength(0); x++)
+            {
+                for (int y = 0; y < borderedMap.GetLength(1); y++)
+                {
+                    if (x >= borderSize && x < widthDungeon + borderSize && y >= borderSize && y < heightDungeon + borderSize)
+                    {
+                        borderedMap[x, y] = worldMap[x - borderSize, y - borderSize];
+                    }
+                    else
+                    {
+                        borderedMap[x, y] = 1;
+                    }
+                }
+            }
+        }
+
+        private void GenerateArrayOfMap(BspTree tree)
+        {
+            if (tree.IsInternal())
+            {
+                if (tree.left != null)
+                {
+                    GenerateArrayOfMap(tree.left);
+                }
+                if (tree.right != null)
+                {
+                    GenerateArrayOfMap(tree.right);
+                }
+            }else if(tree.IsLeaf())
+            {
+                for (int x = 0; x < tree.room.width; x++)
+                {
+                    for (int y = 0; y < tree.room.height; y++)
+                    {
+                        int posX = x + tree.room.x;
+                        int posY = y + tree.room.y;
+                        //Vector2 vec2 = new Vector2(posX, posY);
+
+                        if (posX >= 0 && posX < widthDungeon && posY >= 0 && posY < heightDungeon)
+                        {
+                            worldMap[posX, posY] = 0;
+                            if (x >= 0 && y >= 0 && x < tree.room.width - 1 && y < tree.room.height -1  )
+                            {
+                                worldMap[posX, posY] = 0;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnDrawGizmos()
+        {
+            if (borderedMap != null)
+            {
+                for (int x = 0; x < borderedMap.GetLength(0); x++)
+                {
+                    for (int y = 0; y < borderedMap.GetLength(1); y++)
+                    {
+                       Gizmos.color = borderedMap[x,y] == 0 ? Color.white : Color.black;
+                        Vector3 pos = new Vector3(x, y, 0);
+                        Gizmos.DrawCube(pos, Vector3.one * 0.5f);
+                    }
+                }
+            }
+        }
+        
+        void Update()
+        {
+            if (Input.GetMouseButton(0))
+            {
+                GenerateMap();
+            }
+        }
+    }
+}
+
